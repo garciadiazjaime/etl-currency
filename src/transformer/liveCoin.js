837457db2004902a1ccf4fa87b066d1839c97f49
alex-romanov <alejandro@mintitmedia.com>
2018-05-19 12:26:53 -0600

    error fix

--- src/transformer/liveCoin.js
+++ src/transformer/liveCoin.js
@@ -19,20 +19,11 @@ function transform(props, response) {
   const currencies = quotes.filter(currency => props.currencies.includes(currency.cur) && currency.symbol.includes('/USD'));
 
   const currencyList = currencies.map((item) => {
-    item.currency = item.cur;
-    item.rate = item.last;
-    delete item.cur;
-    delete item.symbol;
-    delete item.last;
-    delete item.high;
-    delete item.low;
-    delete item.volume;
-    delete item.vwap;
-    delete item.max_bid;
-    delete item.min_ask;
-    delete item.best_bid;
-    delete item.best_ask;
-    return item;
+    const newItem = {
+      currency: item.cur,
+      rate: item.last,
+    };
+    return newItem;
   });
 
   return currencyList;
